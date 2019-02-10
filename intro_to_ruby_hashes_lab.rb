c98f26219110c8dedbb6e89f3785f5d7f0b17f11
Emily Miles <emiles4@gmail.com>
2019-02-10 06:06:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,6 @@ def monopoly_with_second_tier
 					:shortline => {
 							:mortgage_value => "$800"
 					}
-			}
 			:rent_in_dollars => {
 					:one_piece_owned => 25,
 					:two_pieces_owned => 50,
