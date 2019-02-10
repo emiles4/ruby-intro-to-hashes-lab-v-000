998ce6f69ecd23eea0015868c5a817a3a00be84e
Emily Miles <emiles4@gmail.com>
2019-02-10 05:55:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads =>{
-			:pieces => 4
+			:pieces => 4,
 			:names => {
 					:reading_railroad => {
 							:mortgage_value => "$100"
